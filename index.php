5ebb9bb545951174f96b058df7e0f78425c9b37a
Miecaela Garcia <76048114+mvsgarcia@users.noreply.github.com>
2021-06-11 11:40:04 +0800

    Add files via upload
    first php file

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+    // echo 'hello world';
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My First PHP File</title>
+</head>
+<body>
+    
+    <h1>
+        <?php echo 'hello world' ?>;
+    </h1>
+
+</body>
+</html>
