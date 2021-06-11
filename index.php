e72b724eecd47248b308ecdc777893907298c194
Miecaela Garcia <76048114+mvsgarcia@users.noreply.github.com>
2021-06-11 12:11:01 +0800

    variables & constants

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
-    // echo 'hello world';
+    define('NAME', 'Maurer');
+    $name = "Mario";
+    $age = 30;
 
 ?>
 
@@ -11,13 +13,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My First PHP File</title>
+    <title>PHP Tutorials</title>
 </head>
 <body>
     
-    <h1>
-        <?php echo 'hello world' ?>;
-    </h1>
+    <h1>User Profile Page</h1>
+
+    <div> <?php echo NAME; ?></div>
+    <div> <?php echo $name; ?> </div>
+    <div> <?php echo $age; ?> </div>
 
 </body>
 </html>
