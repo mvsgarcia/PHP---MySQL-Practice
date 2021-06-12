3e2b21476f53de35a748d6d4e6e0dedd9dfef15d
Miecaela Garcia <76048114+mvsgarcia@users.noreply.github.com>
2021-06-12 18:52:32 +0800

    header & footer

---
+++ templates/header.php
@@ -0,0 +1,34 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mochi's Pizza</title>
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+</head>
+
+  <style type="text/css">
+	.brand{
+	  	background: #cbb09c !important;
+	  }
+  	.brand-text{
+  		color: #cbb09c !important;
+  	}
+  </style>
+
+<body class="grey lighten-4"> <!-- grey bg -->
+
+	<nav class="white z-depth-0"> <!-- white bg, and remove shadow-->
+
+        <div class="container">
+
+            <a href="#" class="brand-logo brand-text center">Mochi's Pizza</a> <!-- logo -->
+
+            <ul id="nav-mobile" class="right hide-on-small-and-down">
+
+                    <li><a href="#" class="btn brand z-depth-0">Add a Pizza</a></li>
+
+            </ul>
+        </div>
+  </nav>
